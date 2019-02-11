50c04a224f699701b34c133b5670820541d167f6
Lucas Prins <lljPrins@avans.nl>
2019-02-11 13:31:07 +0100

    Stuff
    biem

--- Cool Code/src/Main.java
+++ Cool Code/src/Main.java
@@ -1,2 +1,23 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        BorderPane borderPane = new BorderPane();
+        Scene scene = new Scene(borderPane);
+
+        ImageView imageView = new ImageView();
+        imageView.setImage(new Image("rec/low_temperature-512.png"));
+        borderPane.setCenter(imageView);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
